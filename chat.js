452701357b4663aea205a17cd8a40745100d3070
Grosan Flaviu Gheorghe <fgheorghe@grosan.co.uk>
2013-04-22 00:18:52 +0100

    Added security note.

--- chat.js
+++ chat.js
@@ -25,6 +25,8 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+// TODO: Format client names
+
 /**
  * Chat Application Object.
  * @class Provides chat functionality.
